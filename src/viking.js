5c0adeff1285bddabd407237126311a79ccce7bb
[Christian Mueller] <[c-mueller87@live.de]>
2021-11-20 22:24:36 +0100

    done

--- src/viking.js
+++ src/viking.js
@@ -1,14 +1,93 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor (health, strength) {
+    this.health = health
+    this.strength = strength
+  }
+
+  attack() {
+    return this.strength
+  }
+
+  receiveDamage(damage) {
+    this.health -= damage
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor (name, health, strength) {
+    super (health, strength)
+    this.name = name
+  }
+
+    receiveDamage(damage) {
+      this.health -= damage
+      if(this.health > 0) return `${this.name} has received ${damage} points of damage`
+      if(this.health < 1) return `${this.name} has died in act of combat`
+    }
+
+    battleCry() {
+      return `Odin Owns You All!`
+    }
+
+  }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    
+  receiveDamage(damage) {
+    this.health -= damage
+    if(this.health > 0) return `A Saxon has received ${damage} points of damage`
+    if(this.health < 1) return `A Saxon has died in combat`
+  }
+
+}
 
 // War
-class War {}
+class War {
+  constructor(){
+    this.vikingArmy = []
+    this.saxonArmy = []
+  } 
+  
+  
+  
+  addViking(viking) {
+    this.vikingArmy.push(viking)
+  }
+
+  addSaxon(saxon) {
+    this.saxonArmy.push(saxon)
+  }
+
+  vikingAttack () {
+    let saxon = this.saxonArmy[Math.floor(this.saxonArmy.length * Math.random())]
+    let viking = this.vikingArmy[Math.floor(this.vikingArmy.length * Math.random())]
+
+    let remainingHealth = saxon.receiveDamage(viking.strength)
+
+    if(saxon.health <= 0) this.saxonArmy.pop()
+    return remainingHealth
+  }
+
+  saxonAttack() {
+    let saxon = this.saxonArmy[Math.floor(this.saxonArmy.length * Math.random())]
+    let viking = this.vikingArmy[Math.floor(this.vikingArmy.length * Math.random())]
+
+    let remainingHealth = viking.receiveDamage(saxon.strength)
+
+    if(viking.health <= 0) this.vikingArmy.pop()
+    return remainingHealth
+  }
+
+  showStatus() {
+    if(!this.vikingArmy.length) return `Saxons have fought for their lives and survived another day...`
+    if(!this.saxonArmy.length) return `Vikings have won the war of the century!`
+    if(this.vikingArmy.length && this.saxonArmy.length) return `Vikings and Saxons are still in the thick of battle.`
+    
+  }
+}
 
 
 
@@ -17,3 +96,4 @@ class War {}
 if (typeof module !== 'undefined') {
   module.exports = { Soldier, Viking, Saxon, War };
 }
+
